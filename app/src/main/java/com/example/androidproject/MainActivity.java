115dbf1846f3353fc396edaf79e141014bd394f4
Nguyen Trong Hung <nguyentronghung3005@gmail.com>
2021-10-30 10:40:27 +0700

    aaa

---
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -0,0 +1,33 @@
+    package com.example.androidproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+
+    public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+
+
+
+}
